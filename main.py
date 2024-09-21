88edddff10683ebe80c48a13aba3b0d7c1e0d3e2
Santosh Narayanan <santosh33@gmail.com>
2024-09-21 23:49:19 +0200

    create data for API

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating:Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite food", "content": "I like pizza", "id": 2}]
 
 
 @app.get("/")
@@ -10,17 +20,11 @@ async def root():
 
 @app.get("/post")
 async def get_post():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payLoad:dict = Body(...)):
-    print(payLoad)
-    return {
-            "new_post": f"title {payLoad['title']}, content: {payLoad['content']}"        # <--- added closing quote
-        }
+@app.post("/posts")
+def create_posts(new_post:Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post}
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: int | None = None
