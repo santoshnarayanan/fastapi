d7c169a2b028f6f0a39795818a087c2edaa3ce6f
Santosh Narayanan <santosh33@gmail.com>
2024-09-22 02:08:22 +0200

    update code with more api path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException, Response, status
+from httpx import post
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -23,8 +25,27 @@ async def get_post():
     return {"data": my_posts}
 
 @app.post("/posts")
-def create_posts(new_post:Post):
-    print(new_post)
-    print(new_post.dict())
-    return {"data": new_post}
+def create_posts(post:Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"data": post}
+
+@app.get("/posts/{id}")
+def get_post(id:int, response: Response):
+    post = None
+    for p in my_posts:
+        if p["id"] == id:
+            post = p
+    if(not post):
+        # response.status_code = 404
+        # return {"message": f"post with id {id} not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id {id} not found")
+    return {"data": post}
